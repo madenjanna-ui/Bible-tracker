350decf24fa00bb4129469b65d89b8b2d8246995
madenjanna-ui <madenjanna@gmail.com>
2026-08-04 13:43:41 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE = "BibleTracker-v1";
+
+const FILES = [
+
+"/",
+
+"/index.html",
+
+"/style.css",
+
+"/app.js",
+
+"/sync.js",
+
+"/config.js",
+
+"/data.js",
+
+"/manifest.json"
+
+];
+
+self.addEventListener("install", e => {
+
+e.waitUntil(
+
+caches.open(CACHE)
+
+.then(cache => cache.addAll(FILES))
+
+);
+
+});
+
+self.addEventListener("fetch", e => {
+
+e.respondWith(
+
+caches.match(e.request)
+
+.then(r => r || fetch(e.request))
+
+);
+
+});
